6c314eafee89ce1e02d11a3bf1c7320db34a065d
Roberto Martinez <roberto.martinez@claroshop.com>
2022-05-19 00:04:12 -0500

    [NEW] Add code for stori

---
+++ main.py
@@ -0,0 +1,150 @@
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import datetime
+import smtplib
+import json
+import csv
+import pymysql
+import os
+
+
+#Variables entorno
+username = os.environ.get('USER_MAIL', 'robert20.25.escom@gmail.com')
+password = os.environ.get('PASS_MAIL', 'ordsknkmbsobtxgp')
+host=os.environ.get('MYSQL_HOST', 'localhost')
+user=os.environ.get('MYSQL_USER', 'root')
+password=os.environ.get('MYSQL_PASSWORD', 'password')
+db=os.environ.get('MYSQL_DB', 'stori')
+
+
+#Función que lee el archivo csv
+def read_file():
+
+    with open('transactions.csv')as file:
+        reader = csv.reader(file, delimiter=",")
+
+        header = next(file)
+        rows = []
+        for row in reader :
+            rows.append(row)
+
+    save_data(rows)
+
+#FIN read_file
+
+
+#Declara conección a BD mysql
+connection = pymysql.connect(
+        host=host,
+        user=user,
+        password=password,
+        db=db
+    )
+#Función que guarda los datos extraidos del archivo csv
+def save_data(data):
+
+    cursor = connection.cursor()
+    data_insert = []
+    for row in data:
+        date = datetime.strptime(row[1], '%Y/%m/%d')
+        num = float(row[2])
+        data_insert.append([date,num])
+    
+    sql = "INSERT INTO transactions(date,transaction) VALUES (%s, %s)"
+
+    cursor.executemany(sql, data_insert)
+    connection.commit()
+
+#FIN save_data
+
+
+
+#Configuración para envio mail
+content = open('config/email.json')
+config = json.load(content)
+email = config['username']
+password = config['password']
+
+sender = 'robert20.25.escom@gmail.com'
+receiver = 'roberto.martinez@claroshop.com'
+subject = "Balance"
+
+#Función que envía el mail 
+def send_mail():
+    multipart = MIMEMultipart()
+    multipart['Subject'] = 'Balance'
+    multipart['From'] = 'stori@stori.com'
+    multipart['To'] = 'robert20.25.escom@gmail.com'
+   
+    multipart.attach(MIMEText(temp, 'html'))
+ 
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(sender, receiver, multipart.as_string())
+
+    server.quit()
+#FIn send_mail
+
+#Variables para mostrar en template
+total=float
+months = []
+avg_credit = float
+avg_debit = float
+
+#consultas para obtener datos guardados
+cursor = connection.cursor()
+#obtiene el total 
+sql = "SELECT SUM(transaction) FROM transactions" 
+cursor.execute(sql)
+
+res = cursor.fetchone()
+total = res[0]
+total = round(total,2)
+#obtiene el total de transacciones por mes
+sql = "SELECT MONTH(date),COUNT(transaction) FROM transactions GROUP BY MONTH(date) ORDER BY MONTH(date)"
+cursor.execute(sql)
+
+res = cursor.fetchall()
+
+for row in res:
+    date = datetime.strptime(str(row[0]), "%m")
+    full_month_name = date.strftime("%B")
+    num = row[1]
+    months.append([full_month_name,num])
+
+#obtiene el promedio de credito
+sql = "SELECT avg(transaction) FROM transactions where transaction > 0"
+cursor.execute(sql)
+res = cursor.fetchone()
+avg_credit = res[0]
+avg_credit = round(avg_credit,2)
+
+#obtiene el promedio de debito   
+sql = "SELECT avg(transaction) FROM transactions where transaction < 0"
+cursor.execute(sql)
+res = cursor.fetchone()
+avg_debit = res[0]
+avg_debit = round(avg_debit,2)
+
+aux_months = ""
+for i in months:
+    aux_months+= f'''<tr style="font-size:12px; font-weight:bold;">
+                        <td width="50%" style="border:1px solid black; text-align: center;">{i[0]} - {i[1]}</td>
+                    </tr>
+                '''
+#Carga de template donde se le envían las variables a mostrar
+bodytemp = r'template/template.html'
+with open(bodytemp, "r", encoding='utf-8') as f:
+    temp= f.read().format(total = total, avg_credit = avg_credit, avg_debit = avg_debit, months = aux_months)   
+
+
+
+#Llamado a funciones
+
+read_file()
+
+send_mail()
+
+#FIN script
